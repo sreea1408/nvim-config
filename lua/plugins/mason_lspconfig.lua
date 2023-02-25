0ea8760897a8d8f2a02ad13f0aa7bc8362cc1286
sreea1408 <108213270+sreea1408@users.noreply.github.com>
2023-02-25 08:34:39 +0000

    Create minimal nvim config

--- lua/plugins/mason_lspconfig.lua
+++
@@ -1,4 +0,0 @@
-local ok, mason_lspconfig = pcall(require, 'mason-lspconfig')
-if not ok then return end
-
-mason_lspconfig.setup()
--- lua/plugins/notify.lua
+++
@@ -1,8 +0,0 @@
-local ok, notify = pcall(require, 'notify')
-if not ok then return end
-
-notify.setup {
-    stages = 'static',
-}
-
-vim.notify = notify
